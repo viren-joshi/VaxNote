6d5a808d3ad29d76b474e13b4bf2857864da155b
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2021-10-30 10:36:56 +0530

    runnable
    now you can also run the app in your pc and clicking the sign in and sign up buttons will open main activity without user authentication.. so it'll be easy for you to edit the design

--- app/src/main/java/com/example/vaxnote/SignInActivity.java
+++ app/src/main/java/com/example/vaxnote/SignInActivity.java
@@ -30,7 +30,6 @@ public class SignInActivity extends AppCompatActivity {
     EditText userloginEmail,userloginPassword;
     TextView goToSignUp;
     Button SignIn;
-    Intent open_main_activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,8 +55,10 @@ public class SignInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String userLoginEmail = userloginEmail.getText().toString();
                 String userLoginPassword = userloginPassword.getText().toString();
-                UserAuthentication userAuthentication = new UserAuthentication(SignInActivity.this);
-                userAuthentication.AuthenticateUser(userLoginEmail,userLoginPassword);
+//                 UserAuthentication userAuthentication = new UserAuthentication(SignInActivity.this);
+//                 userAuthentication.AuthenticateUser(userLoginEmail,userLoginPassword);
+                Intent open_main_activity = new Intent(SignInActivty.this, MainActivity.class);
+                startActivity(open_main_activity);
             }
         });
     }
