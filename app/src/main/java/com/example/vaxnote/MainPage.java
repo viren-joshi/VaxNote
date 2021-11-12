7714c8ca7cc442edb9986bedb64b6d6b481fa2ce
amitm15 <amitmaity0415@gmail.com>
2021-11-12 21:38:21 +0530

    fixed some things

--- app/src/main/java/com/example/vaxnote/MainPage.java
+++ app/src/main/java/com/example/vaxnote/MainPage.java
@@ -1,24 +1,27 @@
 package com.example.vaxnote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.activity.ComponentActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //Variables
     static DrawerLayout drawerLayout;
@@ -39,23 +42,46 @@ public class MainPage extends AppCompatActivity {
             window.setStatusBarColor(this.getResources().getColor(R.color.statusbar_yellow));
         }
 
-        drawerLayout=findViewById(R.id.drawer_layout);
-        toolbar=findViewById(R.id.toolbar);
-        imageButton=findViewById(R.id.left_tap);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        imageButton = findViewById(R.id.left_tap);
 
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(MainPage.this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainPage.this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
 
         //Drawer
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainPage.this, "Hello", Toast.LENGTH_SHORT).show();
                 MainPage.drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+        switch (menuitem.getItemId()) {
+            case R.id.vac_dic:
+                Intent intent=new Intent(MainPage.this,Vaccineinfo.class);
+                startActivity(intent);
+                 break;
+        }
+
+        return true;
     }
 }
