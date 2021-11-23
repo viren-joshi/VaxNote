f817551d0dd1c8c1c529eb92982e193c90e1cb9a
amitm15 <amitmaity0415@gmail.com>
2021-11-24 01:49:23 +0530

    All pages created, some layouts i cant figure out

---
+++ app/src/main/java/com/example/vaxnote/activities/new_record.java
@@ -0,0 +1,16 @@
+package com.example.vaxnote.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vaxnote.R;
+
+public class new_record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_record);
+    }
+}
---
+++ app/src/main/java/com/example/vaxnote/activities/vac_record.java
@@ -0,0 +1,16 @@
+package com.example.vaxnote.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vaxnote.R;
+
+public class vac_record extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vac_record);
+    }
+}
