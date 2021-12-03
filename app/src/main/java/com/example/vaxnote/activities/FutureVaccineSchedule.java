069cfb913535fe3ab5abf72aba9449f5091c2072
Viren Joshi <viren.joshi403@gmail.com>
2021-12-03 20:02:23 +0530

    Fixes

--- app/src/main/java/com/example/vaxnote/activities/FutureVaccineSchedule.java
+++ app/src/main/java/com/example/vaxnote/activities/FutureVaccineSchedule.java
@@ -1,21 +1,18 @@
 package com.example.vaxnote.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.vaxnote.R;
 import com.example.vaxnote.classes.LocalInfo;
 import com.example.vaxnote.classes.NotificationAdapter;
-import com.example.vaxnote.fragments.NewRecordFragment;
 
 import java.util.ArrayList;
 
